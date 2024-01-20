8c8290231acfb875922ea39bd7b3234450eb2e2b
Fadhil Ahmad <fadzbobby95@gmail.com>
2024-01-20 19:53:49 +0800

    feat: better output

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,30 @@ import fetch from "node-fetch";
 
 async function findSupportedVersion(packageName: string, nodeVersion: string) {
   try {
+    const valid = semver.valid(semver.coerce(nodeVersion));
+    if (!valid) return console.log("Invalid version");
+
+    console.log(
+      `Finding version of package '${packageName}' that satisfies node engine of ${valid}`
+    );
     const response = await fetch(`https://registry.npmjs.org/${packageName}`);
     const data: any = await response.json();
 
     const versions = Object.keys(data.versions);
 
-    console.log("Object.keys(data)", Object.keys(data));
+    let redCompared: string[] = [];
     for (const version of versions
       .reverse()
       .filter((f) => f.indexOf("-") < 0)) {
-      console.log(`Checking ${packageName}@${version}`);
       const engines = data.versions[version].engines;
-      if (semver.satisfies(nodeVersion, engines.node)) {
+      if (!engines || redCompared.find((f) => f === engines.node)) continue;
+
+      console.log(`Checking ${packageName}@${version}: ${engines.node}`);
+      if (valid && semver.satisfies(valid, engines.node)) {
         console.log(`Compatible version found: ${packageName}@${version}`);
         return;
       }
+      redCompared.push(engines.node);
     }
     console.log("No compatible versions found");
   } catch (error) {
